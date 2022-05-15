4ba3ced539b41b670707b2b281dcfa74fe3cfde1
Dong <dongchenyu164@gmail.com>
2022-05-15 13:09:23 +0900

    Initial commit

---
+++ Source/TestCompile/TestCompile.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestCompile.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestCompile, "TestCompile" );
---
+++ Source/TestCompile/TestCompileGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestCompileGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCOMPILE_API ATestCompileGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
